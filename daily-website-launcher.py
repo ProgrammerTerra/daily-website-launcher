cc99fca584a99b4ea9317da0ae383a7ed95bba51
Terra Miller <programmerterra@gmail.com>
2025-01-13 14:17:38 -0500

    Allows the user to automatically open a predetermined list of URLs in their browser

--- daily-website-launcher.py
+++ daily-website-launcher.py
@@ -1,45 +1,55 @@
 import webbrowser
 from datetime import datetime
 
-# list of URLs you want to open in your browser everyday
-urls = [
-    "https://twitter.com",
-]
+urls = []
+
+# Update this to the path of your chosen browser
+browser_path = "C:/Program Files/Mozilla Firefox/firefox.exe"
+
+
+
+# list of URLs you want to open in your browser every day
+urls.append("https://github.com/ProgrammerTerra")
+urls.append("https://google.com")
 
 #Monday
 if datetime.today().weekday() == 0:
-    urls.append("https://twitter.com") #Twitter
+    urls.append("https://wikipedia.org")
+    urls.append("https://coursera.org")
     
 #Tuesday
 elif datetime.today().weekday() == 1:
-	urls.append("https://twitter.com")
+    urls.append("https://stackoverflow.com")
+    urls.append("https://www.theodinproject.com/")
     
 #Wednesday
 elif datetime.today().weekday() == 2:
-    urls.append("https://twitter.com")
+    urls.append("https://www.khanacademy.org/")
+    urls.append("https://brilliant.org")
     
 #Thursday
 elif datetime.today().weekday() == 3:
-    urls.append("https://twitter.com")
+    urls.append("https://leetcode.com/explore/")
+    urls.append("https://www.skillshare.com/en/")
     
 #Friday
 elif datetime.today().weekday() == 4:
-    urls.append("https://twitter.com")
+    urls.append("https://www.npr.org/")
+    urls.append("https://www.goodreads.com/")
     
 #Saturday
 elif datetime.today().weekday() == 5:
-    urls.append("https://twitter.com")
+    urls.append("https://netflix.com")
+    urls.append("ed.ted.com")
     
 #Sunday
 elif datetime.today().weekday() == 6:
-    urls.append("https://twitter.com")
+    urls.append("https://duolingo.com")
+    urls.append("https://www.headspace.com/")
 
 
-# Set Firefox as the browser
-firefox_path = "C:/Program Files/Mozilla Firefox/firefox.exe"
-chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
-webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 
 # Open each URL in a new tab
+webbrowser.register('browser', None, webbrowser.BackgroundBrowser(browser_path))
 for url in urls:
-    webbrowser.get('chrome').open_new_tab(url)
+    webbrowser.get('browser').open_new_tab(url)
---
+++ readme.md
@@ -0,0 +1,15 @@
+# daily-website-launcher
+
+I haven't always been as productive as I am today. I had to figure out the right combination of tools, schedules, and habits that work for my brain, which will be different from what works for someone else. One of the major themes of my system is making things "frictionless." I need to make certain repeated tasks as simple as possible, to make them something I don't even have to think about. One of the ways I've been able to achieve this is by having websites that I use regularly automatically open, whether that be daily, weekly, every other day, or any other combination I may need. This script helps me do exactly that. For example, I listen to 3 podcasts regularly, but I have never been a fan of RSS feeds or other similar technology. So on Fridays, I have the websites for all 3 of the podcasts open up in a new tab. 
+
+This has been an absolute game-changer for me. If this sounds like something you could use for yourself try it out! I hope it makes your weeks feel just a little bit easier to tackle. 
+
+# what this script does
+
+Automatically opens a predetermined list of URLs in your browser.
+
+To get this script to work you'll need to - 
+1. Update the **firefox_path** variable to the location of your chosen browser. 
+2. Schedule the script to run at the times you want. This can be done using the windows task scheduler, apple automator, or with cron commands on linux.
+
+<3 Terra
